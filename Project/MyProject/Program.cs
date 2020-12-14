4e0bab14091794a0f9a7794d89603d7c1dbeca3b
Patty R <patty.ramert@gmail.com>
2020-12-14 09:57:13 -0800

    init

---
+++ Project/MyProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            var library = new MyProjectLibrary.Function();
+            library.SayHi();
+        }
+    }
+}
